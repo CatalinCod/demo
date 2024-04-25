6b182221ed041e36663266b10d86cec9c40952b3
avictor25 <autolivsrl@gmail.com>
2024-04-25 13:24:56 +0300

    Initial commit

---
+++ src/main/java/com/examplu/PersoanaMapper.java
@@ -0,0 +1,11 @@
+package com.examplu;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+public class PersoanaMapper implements RowMapper<Persoana>{
+    @Override
+    public Persoana mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Persoana(rs.getInt("id"), rs.getString("nume"), rs.getInt("varsta"));
+    }
+}
---
+++ src/test/java/com/examplu/ExempluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExempluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
